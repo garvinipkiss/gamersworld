d3ac4aa18b03cffc4dae18181622ed089570c306
garvinipkiss <garvinipkiss@gmail.com>
2018-04-23 09:02:39 +0300

    test files

--- config.py
+++ config.py
@@ -1,11 +1,45 @@
+import os
+
 class Config:
-    pass
+
+    MOVIE_API_BASE_URL ='https://www.igdb.com/api/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('GAME_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'gamers'
+    SENDER_EMAIL = 'garvinipkiss@gmail.com'
+
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
+
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://garvin:0000@localhost/gamers_test'
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://garvin:0000@localhost/gamers'
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig}
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 
+}
